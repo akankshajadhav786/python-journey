5c22e9b43e7b34aeb6afb94369eb348d29a51d3b
akankshajadhav786 <jadhavakanksha473@gmail.com>
2026-01-06 23:32:05 +0530

    Day3 - python learning

---
+++ Day3.py
@@ -0,0 +1,15 @@
+# DAY3 PYTHON JOURNEY
+#if-else conditionals 
+print("Welcome to rollercoaster")
+height=int(input("Enter the height of candidate in cm"))
+if height<180:
+    print("eligible to ride")
+else:
+    print("Not eligible to ride")    
+
+#even odd
+number=int(input("Enter the number"))
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
